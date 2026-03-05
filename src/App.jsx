145bfcdab305749a946d9b5a271f2e908bc954e5
Olga Sessions <olenkafirst@gmail.com>
2026-03-04 22:03:22 -0500

    workouts form list about placeholders layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Workouts from "./pages/Workouts";
+import About from "./pages/About";
 import "./App.css";
 
 function App() {
-  return <div>{/* {workouts app will render here} */}</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Workouts />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <header style={{ padding: "1rem", background: "#f0f0f0" }}>
+        <h1 style={{ margin: 0 }}>Daily Fitness </h1>
+        <nav style={{ marginTop: "0.5rem" }}>
+          <NavLink to="/" end style={{ marginRight: "1rem", color: "black" }}>
+            Workouts
+          </NavLink>
+          <NavLink to="/about" style={{ color: "black" }}>
+            About
+          </NavLink>
+        </nav>
+      </header>
+      <main style={{ padding: "1rem" }}>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
