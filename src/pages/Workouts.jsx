8cc0211a716b3f991a3512aebb9bf2218559145a
Olga Sessions <olenkafirst@gmail.com>
2026-03-09 09:30:25 -0400

    local storage workouts.jsx

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
-
-import WorkoutList from "../components/WorkoutList";
+import { useState, useEffect } from "react";
 import WorkoutForm from "../components/WorkoutForm";
+import WorkoutList from "../components/WorkoutList";
 
 export default function Workouts() {
   const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    const storedWorkouts = localStorage.getItem("workouts");
+    if (storedWorkouts) setWorkouts(JSON.parse(storedWorkouts));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+  }, [workouts]);
 
   const addWorkout = (workout) => {
     const newItem = { id: Date.now().toString(), ...workout };
